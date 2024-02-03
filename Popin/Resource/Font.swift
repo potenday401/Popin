979f1a0211fb21a9df1c499d645c1a08b32b3aae
Anniekkang <knr9144@gmail.com>
2024-02-04 05:11:17 +0900

    feat: make Color and Font resources

---
+++ Popin/Resource/Font.swift
@@ -0,0 +1,14 @@
+//
+//  Font.swift
+//  Popin
+//
+//  Created by 나리강 on 2/4/24.
+//
+
+import UIKit
+
+ enum variousFont {
+     static let textFieldFont = UIFont.systemFont(ofSize: 16, weight: .medium)
+     static let buttonFont = UIFont.systemFont(ofSize: 16, weight: .bold)
+     static let labelFont = UIFont.systemFont(ofSize: 14, weight: .bold)
+ }
