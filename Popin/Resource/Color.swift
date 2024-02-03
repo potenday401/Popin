d4cc0f8afb41727dc14c96ae8c8a37feb67577db
Anniekkang <knr9144@gmail.com>
2024-02-04 05:11:58 +0900

    feat: arrange Color resources

--- Popin/Resource/Color.swift
+++ Popin/Resource/Color.swift
@@ -14,12 +14,4 @@ import UIKit
 
      let buttonColor = UIColor(red: 94/255, green: 92/255, blue: 230/255, alpha: 1)
      let textFieldColor = UIColor(red: 72/255, green: 72/255, blue: 74/255, alpha: 1)
-
-     let Yellow = UIColor(red: 244/255, green: 164/255, blue: 66/255, alpha: 1)
-     let darkGray = UIColor(red: 148/255, green: 148/255, blue: 148/255, alpha: 1)
-     let backgroundGray = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
-     let lineGray = UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1)
-     let black = UIColor.black
-     let white = UIColor.white
-
  }
