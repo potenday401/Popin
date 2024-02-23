d5758f41e7dc3886bb0a17d03167f02ad6d3c141
Chamsol Kim <kcsol1005@gmail.com>
2024-02-23 21:15:27 +0900

    feat: design system color 추가

---
+++ Popin/UI Components/Color/UIColor+PDS.swift
@@ -0,0 +1,51 @@
+//
+//  UIColor+PDS.swift
+//  Popin
+//
+//  Created by chamsol kim on 2/23/24.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    // MARK: - Grayscale
+    
+    static let gray100 = UIColor(hex: 0x848794)
+    static let gray200 = UIColor(hex: 0x565D68)
+    static let gray300 = UIColor(hex: 0x3D444F)
+    static let gray400 = UIColor(hex: 0x212730)
+    static var gray500 = UIColor(hex: 0x11161F)
+    static var gray600 = UIColor(hex: 0x0A0E18)
+    static var gray700 = UIColor(hex: 0x0A0514)
+    
+    static let lightGray100 = UIColor(hex: 0xF2F2F7)
+    static let lightGray200 = UIColor(hex: 0xE5E5EA)
+    static let lightGray300 = UIColor(hex: 0xD1D1D6)
+    static let lightGray400 = UIColor(hex: 0xC7C7CC)
+    static var lightGray500 = UIColor(hex: 0xAEAEB2)
+    static var lightGray600 = UIColor(hex: 0x8E8E93)
+    
+    static let darkGray100 = UIColor(hex: 0x8E8E93)
+    static let darkGray200 = UIColor(hex: 0x636366)
+    static let darkGray300 = UIColor(hex: 0x48484A)
+    static let darkGray400 = UIColor(hex: 0x3A3A3C)
+    static let darkGray500 = UIColor(hex: 0x2C2C2E)
+    static let darkGray600 = UIColor(hex: 0x1C1C1E)
+    
+    // MARK: - Color
+    
+    static let indigo100 = UIColor(hex: 0x7D7AFF)
+    static let indigo200 = UIColor(hex: 0x5E5CE6)
+    static let indigo300 = UIColor(hex: 0x3634A3)
+    
+    static let purple100 = UIColor(hex: 0xDA8FFF)
+    static let purple200 = UIColor(hex: 0xBF5AF2)
+    static let purple300 = UIColor(hex: 0x8944AB)
+    
+    static let pink100 = UIColor(hex: 0xFF6482)
+    static let pink200 = UIColor(hex: 0xFF2D55)
+    static let pink300 = UIColor(hex: 0xD30F45)
+    
+    static let yellow100 = UIColor(hex: 0xF8FC3C)
+}
