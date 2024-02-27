d3b3b88f51e69128fa9a6e582bfc42066dcf9bab
Chamsol Kim <kcsol1005@gmail.com>
2024-02-27 20:45:52 +0900

    test [#15] 인증번호 입력 component preview 보완
    Preview에서는 text field에 직접 입력할 수 없으므로 간단한 테스트만 진행합니다.
    로그인 화면을 개발할 때 실제 입력 테스트를 할 수 있습니다.

--- Popin/UI Components/VerificationCodeInput/VerificationCodeInputField+Preview.swift
+++ Popin/UI Components/VerificationCodeInput/VerificationCodeInputField+Preview.swift
@@ -10,21 +10,62 @@ import SnapKit
 
 fileprivate final class VerificationCodeInputFieldPreviewViewController: UIViewController {
     
-    private let inputField = VerificationCodeInputField(numberOfDigits: 5)
+    private lazy var failureButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Set Failure", for: .normal)
+        button.setTitle("Reset", for: .selected)
+        button.addTarget(self, action: #selector(buttonDidTap(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    private let verificationCodeInputField = VerificationCodeInputField(numberOfDigits: 5)
+    
+    private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .gray700
         
-        view.addSubview(inputField)
-        inputField.snp.makeConstraints { make in
+        view.addSubview(stackView)
+        stackView.snp.makeConstraints { make in
             make.center.equalToSuperview()
         }
+        
+        [failureButton, verificationCodeInputField].forEach(stackView.addArrangedSubview(_:))
+        
+        verificationCodeInputField.setNumber("12345")
+    }
+    
+    @objc
+    private func buttonDidTap(_ button: UIButton) {
+        button.isSelected.toggle()
+        verificationCodeInputField.isFailure.toggle()
     }
 }
 
 
 #Preview {
     VerificationCodeInputFieldPreviewViewController()
+}
+
+fileprivate extension VerificationCodeInputField {
+    
+    func setNumber(_ number: String) {
+        let digits = subviews
+            .compactMap { $0 as? UIStackView }
+            .compactMap { $0.arrangedSubviews }
+            .flatMap { $0 }
+            .compactMap { $0 as? VerificationCodeDigit }
+        
+        zip(number.map(String.init), digits).forEach { number, digit in
+            digit.setNumber(number)
+        }
+    }
 }
