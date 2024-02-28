176145b90082c93e849283f4c37a34cdbc69bd72
Chamsol Kim <kcsol1005@gmail.com>
2024-02-28 20:11:39 +0900

    config [#14] 'Common' 파일 정리

--- Popin/Sources/Common/Error/AlertManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AlertManager.swift
-//  fourpin
-//
-//  Created by Jihaha kim on 2024/01/30.
-//
-
-import Foundation
--- Popin/Sources/Common/Extensions/Array +.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Array +.swift
-//  fourpin
-//
-//  Created by Jihaha kim on 2024/01/30.
-//
-
-import Foundation
