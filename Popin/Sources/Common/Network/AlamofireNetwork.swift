d0280d99bdfe6b4d2455d7bb743e2a99da6344eb
Chamsol Kim <kcsol1005@gmail.com>
2024-03-03 19:05:46 +0900

    feat [#17] Alamofire를 사용하는 Network 구현체 개발

---
+++ Popin/Sources/Common/Network/AlamofireNetwork.swift
@@ -0,0 +1,61 @@
+//
+//  AlamofireNetwork.swift
+//  Popin
+//
+//  Created by chamsol kim on 3/3/24.
+//
+
+import Foundation
+import Alamofire
+
+final class AlamofireNetwork: Network {
+    
+    // MARK: - Interface
+    
+    func send<T>(
+        _ request: T,
+        completion: @escaping (Result<Response<T.Output>, Error>) -> Void
+    ) where T : Request, T.Output : Decodable {
+        session
+            .request(
+                request.endpoint,
+                method: convert(from: request.method),
+                parameters: request.query,
+                encoding: JSONEncoding.default
+            )
+            .responseDecodable(of: T.Output.self) { dataResponse in
+                switch dataResponse.result {
+                case .success(let output):
+                    let response = Response(
+                        output: output,
+                        statusCode: dataResponse.response?.statusCode ?? 200
+                    )
+                    completion(.success(response))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+    
+    // MARK: - Property
+    
+    private let session: Session
+    
+    // MARK: - Initializer
+    
+    init(session: Session = .default) {
+        self.session = session
+    }
+}
+
+// MARK: - Converter
+
+private extension AlamofireNetwork {
+    
+    func convert(from method: HTTPMethod) -> Alamofire.HTTPMethod {
+        switch method {
+        case .get:  .get
+        case .post: .post
+        }
+    }
+}
