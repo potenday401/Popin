e335bb0be45d1dce151d4f092870a724e3a2801b
Chamsol Kim <kcsol1005@gmail.com>
2024-03-03 15:46:40 +0900

    refactor [] 불필요한 Network 코드 삭제

--- Popin/Sources/Common/Network/Endpoint.swift
+++ Popin/Sources/Common/Network/Endpoint.swift
@@ -7,16 +7,6 @@
 
 import Foundation
 
-protocol URLRequestConfigurable {
-    var urlString: String { get }
-    var path: String? { get }
-    var method: HTTPMethod { get }
-    var headers: HTTPHeaders? { get }
-    var encoder: ParameterEncodable { get }
-    
-    func asURLRequest() throws -> URLRequest
-}
-
 struct Endpoint { }
 extension Endpoint {
     enum Pin {
@@ -32,50 +22,3 @@ extension Endpoint {
         static let month: String = "&month="
     }
 }
-
-
-//extension Endpoint.Pin: URLRequestConfigurable {
-//
-//    var urlString: String {
-//        return Endpoint.Pin.baseURL
-//    }
-//
-//    var path: String? {
-//        return "/"
-//    }
-//
-//    var method: HTTPMethod {
-//        return .post
-//    }
-//
-//    var headers: HTTPHeaders? {
-//        return ["Content-Type": "application/json"]
-//    }
-//
-//    var encoder: ParameterEncodable {
-//        return URLEncoding()
-//    }
-//
-//    var parameters: Parameters? {
-//        var commonParameters: Parameters = [
-//            "api_key": NetworkConstant.APIKey,
-//        ]
-//
-//        return commonParameters
-//    }
-//
-//    func asURLRequest() throws -> URLRequest {
-//        guard var url: URL = URL(string: self.urlString) else {
-//            throw JHNetworkError.invalidURLString
-//        }
-//
-//        if let path { url.append(path: path) }
-//
-//        var urlRequest: URLRequest = .init(url: url)
-//        urlRequest.httpMethod = self.method.uppercasedValue
-//        urlRequest.allHTTPHeaderFields = self.headers
-//
-//        let encodedRequest = try encoder.encode(request: urlRequest, with: parameters)
-//        return encodedRequest
-//    }
-//}
