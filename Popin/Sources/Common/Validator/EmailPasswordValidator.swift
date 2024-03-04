6089fff03edcbbe6d7185d85b7b3aa167206e352
Chamsol Kim <kcsol1005@gmail.com>
2024-03-04 19:16:37 +0900

    feat [#17] Email 및 password 정규식 검증 객체 개발
    이 객체는 회원가입/비밀번호 찾기까지 모두 개발한 뒤 필요한 곳에서 사용될 예정입니다.

---
+++ Popin/Sources/Common/Validator/EmailPasswordValidator.swift
@@ -0,0 +1,26 @@
+//
+//  EmailPasswordValidator.swift
+//  Popin
+//
+//  Created by chamsol kim on 3/4/24.
+//
+
+import Foundation
+
+struct EmailPasswordValidator {
+    
+    // MARK: - Interface
+    
+    func isValidEmail(_ email: String) -> Bool {
+        email.wholeMatch(of: emailRegex) != nil
+    }
+    
+    func isValidPassword(_ password: String) -> Bool {
+        password.wholeMatch(of: passwordRegex) != nil
+    }
+    
+    // MARK: - Property
+    
+    private let emailRegex = /^(?=.{4,255}$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    private let passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])[A-Za-z\d~`!@#\$%^&*()_+\-=\{\[\}\]|\\:;"'<,>\.?\/]{8,20}$/
+}
---
+++ PopinTests/Common/Validator/EmailPasswordValidatorTests.swift
@@ -0,0 +1,60 @@
+//
+//  EmailPasswordValidatorTests.swift
+//  PopinTests
+//
+//  Created by chamsol kim on 3/4/24.
+//
+
+import XCTest
+@testable import Popin
+
+final class EmailPasswordValidatorTests: XCTestCase {
+    
+    private let validEmailTestCases = [
+        "example@example.com",
+        "user123@example.co.uk",
+        "user.name@example-domain.com",
+    ]
+    
+    private let invalidEmailTestCases = [
+        "example@example",
+        "user@.com",
+        "user@domain",
+    ]
+    
+    private let validPasswordTestCase = [
+        "Password123@",
+        "StrongP@ssw0rd",
+        "Pa$$w0rd123",
+    ]
+    
+    private let invalidPasswordTestCase = [
+        "weakpassword",
+        "12345678",
+        "Password",
+    ]
+    
+    func testValidEmail() {
+        let validator = EmailPasswordValidator()
+        let results = validEmailTestCases.map(validator.isValidEmail(_:))
+        XCTAssertTrue(results.allSatisfy({ $0 }))
+    }
+    
+    func testInvalidEmail() {
+        let validator = EmailPasswordValidator()
+        let results = invalidEmailTestCases.map(validator.isValidEmail(_:))
+        XCTAssertFalse(results.allSatisfy({ $0 }))
+    }
+    
+    func testValidPassword() {
+        let validator = EmailPasswordValidator()
+        let results = validPasswordTestCase.map(validator.isValidPassword(_:))
+        XCTAssertTrue(results.allSatisfy({ $0 }))
+    }
+    
+    func testInvalidPassword() {
+        let validator = EmailPasswordValidator()
+        let results = invalidPasswordTestCase.map(validator.isValidPassword(_:))
+        XCTAssertFalse(results.allSatisfy({ $0 }))
+    }
+}
