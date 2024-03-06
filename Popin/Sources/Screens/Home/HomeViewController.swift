05a929f29666351704b5b0dbe61aa4c447d930c7
김지하 <kimjiha1112@gmail.com>
2024-03-06 15:34:31 +0900

    feat [#30] 지도뷰에 topNaivgaiton 추가

--- Popin/Sources/Screens/Home/HomeViewController.swift
+++ Popin/Sources/Screens/Home/HomeViewController.swift
@@ -8,10 +8,41 @@
 import UIKit
 import Tabman
 import Pageboy
+import SnapKit
 
 class HomeViewController: TabmanViewController {
     
     private var viewControllers: [UIViewController] = []
+    
+    private let navigationBar: PDSNavigationBar = {
+        let navigationBar = PDSNavigationBar()
+        navigationBar.titleView = UIImageView(image: UIImage(resource: .logo))
+        
+        let leftImageView = UIImageView(image: UIImage(resource: .cameraButton))
+        let leftItem = PDSNavigationBarItem()
+        leftItem.addSubview(leftImageView)
+        leftImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(CGSize(width: 24, height: 24))
+            make.left.equalToSuperview().offset(12)
+        }
+        leftImageView.contentMode = .scaleAspectFit
+        navigationBar.leftItem = leftItem
+        
+        let rightImageView = UIImageView(image: UIImage(resource: .profileButton))
+        let rightItem = PDSNavigationBarItem()
+        rightItem.addSubview(rightImageView)
+        rightImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(CGSize(width: 24, height: 24))
+            make.right.equalToSuperview().offset(-12)
+        }
+        rightImageView.contentMode = .scaleAspectFit
+        navigationBar.rightItem = rightItem
+        
+        return navigationBar
+    }()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +54,9 @@ class HomeViewController: TabmanViewController {
         viewControllers.append(dateViewController)
         
         let bar = TMBar.ButtonBar()
-        addBar(bar, dataSource: self, at: .top)
+        
+        //  Mark - todo: 태그, 날짜뷰 추가시 탭 네비게이션 사용
+        //  addBar(bar, dataSource: self, at: .top)
         bar.layout.transitionStyle = .snap
         bar.layout.contentMode = .fit
         bar.backgroundView.style = .clear
@@ -42,6 +75,12 @@ class HomeViewController: TabmanViewController {
         cameraButton.addTarget(self, action: #selector(cameraButtonTapped), for: .touchUpInside)
         
         view.addSubview(cameraButton)
+        view.addSubview(navigationBar)
+        
+        navigationBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.trailing.equalToSuperview()
+        }
         
         cameraButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
@@ -78,4 +117,8 @@ extension HomeViewController: TMBarDataSource {
         let title = index == 0 ? "지도뷰" : "날짜뷰"
         return TMBarItem(title: title)
     }
+}
+
+#Preview {
+     HomeViewController()
 }
