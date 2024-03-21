d10debf130ad2365e4cb0796bc753139bc0767be
김지하 <kimjiha1112@gmail.com>
2024-03-21 17:09:04 +0900

    fix [#40] UploadEwquestDTO 생성

--- Popin/Sources/Screens/Home/Camera/Network/CameraRequest.swift
+++ Popin/Sources/Screens/Home/Camera/Network/CameraRequest.swift
@@ -5,4 +5,66 @@
 //  Created by Jihaha kim on 2024/03/21.
 //
 
-import Foundation
+import CoreLocation
+import UIKit
+
+struct UploadRequestDTO: Encodable, Request {
+    typealias Output = UploadPinResponse
+    var endpoint = Endpoint.Pin.uploadPin.url
+    var method: HTTPMethod = .post
+    var query: QueryItems
+    var header: HTTPHeader = [:]
+    
+    var initialLocation: CLLocation?
+    var selectedPhoto: UIImage?
+    var capturedPhoto: UIImage?
+    var memberId: String
+    var locality: String
+    var subLocality: String
+    var photoDateTime: Int
+    var photoPinId: String
+    var tagIds: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case initialLocation
+        case selectedPhoto
+        case capturedPhoto
+        case memberId
+        case locality
+        case subLocality
+        case photoDateTime
+        case photoPinId
+        case tagIds
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        if let initialLocation = initialLocation {
+            let locationString = "\(initialLocation.coordinate.latitude),\(initialLocation.coordinate.longitude)"
+            try container.encode(locationString, forKey: .initialLocation)
+        }
+        
+        if let selectedPhoto = selectedPhoto, let imageData = selectedPhoto.jpegData(compressionQuality: 0.8) {
+            let base64String = imageData.base64EncodedString()
+            try container.encode(base64String, forKey: .selectedPhoto)
+        }
+        
+        if let capturedPhoto = capturedPhoto, let imageData = capturedPhoto.jpegData(compressionQuality: 0.8) {
+            let base64String = imageData.base64EncodedString()
+            try container.encode(base64String, forKey: .capturedPhoto)
+        }
+        
+        try container.encode(memberId, forKey: .memberId)
+        try container.encode(locality, forKey: .locality)
+        try container.encode(subLocality, forKey: .subLocality)
+        try container.encode(photoDateTime, forKey: .photoDateTime)
+        try container.encode(photoPinId, forKey: .photoPinId)
+        try container.encode(tagIds, forKey: .tagIds)
+    }
+}
+
+
+struct UploadPinResponse: Decodable {
+    let result: String
+}
