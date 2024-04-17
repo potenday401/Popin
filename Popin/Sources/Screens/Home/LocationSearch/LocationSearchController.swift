9b9bb13d2c76740b54d885d97209481ab3c11e5f
김지하 <kimjiha1112@gmail.com>
2024-04-17 15:43:10 +0900

    feat [#23] 장소 검색 화면에 상단 네비게이션 추가

--- Popin/Sources/Screens/Home/LocationSearch/LocationSearchController.swift
+++ Popin/Sources/Screens/Home/LocationSearch/LocationSearchController.swift
@@ -7,16 +7,36 @@
 
 import UIKit
 import MapKit
- 
+
 class LocationSearchController: UIViewController {
     private let searchCompleter = MKLocalSearchCompleter()
     private var searchResults = [MKLocalSearchCompletion]()
     private let searchBar = UISearchBar()
+    private let searchBarImage = UIImage()
     private let containerView = UIView()
     private let mapView = MKMapView()
-
+    private let navigationBar: PDSNavigationBar = {
+        let navigationBar = PDSNavigationBar()
+        navigationBar.title = "장소"
+        return navigationBar
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(navigationBar)
+        navigationBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.trailing.equalToSuperview()
+        }
+        navigationBar.leftItem = .init(
+            image: UIImage(resource: .chevronLeft),
+            target: self,
+            action: #selector(backDidTap)
+        )
+        navigationBar.rightItem = .init(
+            title: "저장",
+            target: self,
+            action: #selector(sendContents)
+        )
         setupSearchBar()
         searchCompleter.delegate = self
         setupResultMap()
@@ -26,15 +46,19 @@ class LocationSearchController: UIViewController {
     private func setupSearchBar() {
         searchBar.placeholder = "장소 검색 (ex. 강남역)"
         searchBar.delegate = self
-        searchBar.showsCancelButton = true
+        searchBar.showsCancelButton = false
+        searchBar.backgroundColor = .clear
+        searchBar.backgroundImage = searchBarImage
+        searchBar.searchTextField.textColor = .white
         view.addSubview(searchBar)
         
         searchBar.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.top.equalTo(navigationBar.snp.bottom)
             make.leading.equalTo(view.snp.leading)
             make.trailing.equalTo(view.snp.trailing)
             make.height.equalTo(50)
         }
+        searchBar.backgroundColor = .black
     }
     
     private func setupResultMap() {
@@ -46,34 +70,41 @@ class LocationSearchController: UIViewController {
             make.height.equalTo(50)
         }
         view.addSubview(mapView)
-          mapView.snp.makeConstraints { make in
+        mapView.snp.makeConstraints { make in
             make.top.equalTo(containerView.snp.bottom).offset(10)
             make.centerX.equalToSuperview()
             make.width.equalTo(375)
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
-          }
+        }
     }
     
     private func geocodeSearchText(_ searchText: String) {
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(searchText) { placemarks, error in
-          if let error = error {
-            print(error.localizedDescription)
-            return
-          }
-          
-          guard let placemark = placemarks?.first else {
-            return
-          }
-          
-          let annotation = MKPointAnnotation()
-          annotation.coordinate = placemark.location!.coordinate
-          annotation.title = searchText
-          self.mapView.addAnnotation(annotation)
-          self.mapView.showAnnotations([annotation], animated: true)
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                return
+            }
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = placemark.location!.coordinate
+            annotation.title = searchText
+            self.mapView.addAnnotation(annotation)
+            self.mapView.showAnnotations([annotation], animated: true)
         }
-      }
-
+    }
+    @objc
+    private func backDidTap() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc
+    private func sendContents() {
+    }
 }
 
 extension LocationSearchController: UISearchBarDelegate {
