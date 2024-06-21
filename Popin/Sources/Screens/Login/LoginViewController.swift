7dfa5b6e040a76e6df610d185ada41e0bee28b36
김지하 <kimjiha1112@gmail.com>
2024-06-21 16:22:22 +0900

    fix [#69] 마이페이지에 내 이메일 보이도록 수정

--- Popin/Sources/Screens/Login/LoginViewController.swift
+++ Popin/Sources/Screens/Login/LoginViewController.swift
@@ -209,7 +209,6 @@ private extension LoginViewController {
                             print("유효한 JSON 형식이 아닙니다.")
                             return
                         }
-                        print(json, "token check")
                         if let responseData = json["responseData"] as? [String: Any] {
                             if let accessToken = responseData["accessToken"] as? String,
                                let refreshToken = responseData["refreshToken"] as? String {
@@ -219,6 +218,7 @@ private extension LoginViewController {
                                 )
                                 TokenManager.shared.accessToken = accessToken
                                 TokenManager.shared.refreshToken = refreshToken
+                                TokenManager.shared.Email = email
 
                                 self.router?.routeToHome(accessToken: accessToken, refreshToken: refreshToken)
                             } else {
